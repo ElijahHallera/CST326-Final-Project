285a0706a785fdaa73603f06dbffc03dda4db761
ElijahHallera <elienzo2929@gmail.com>
2021-04-28 15:07:27 -0700

    Main Menu Scene
    Created a Main menu that is Skyrim Like, Has menu music, Particle Effects, working buttons, and working scene changing

---
+++ Assets/Scripts/MainMenu/ButtonController.cs
@@ -0,0 +1,52 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.EventSystems;
+using UnityEngine.UI;
+using UnityEngine.SceneManagement;
+
+public class ButtonController : MonoBehaviour, IPointerEnterHandler, IPointerExitHandler, IPointerClickHandler
+{
+
+    public Color active;
+    public Color normal;
+    public ButtonType type;
+
+    public enum ButtonType
+    {
+        NEW, CREDITS, QUIT, RETURN
+    }
+
+    public void OnPointerEnter(PointerEventData eventData)
+    {
+        gameObject.GetComponent<Text>().color = active;
+    }
+
+    public void OnPointerExit(PointerEventData eventData)
+    {
+        gameObject.GetComponent<Text>().color = normal;
+    }
+
+    public void OnPointerClick(PointerEventData eventData)
+    {
+        switch (type)
+        {
+            case ButtonType.NEW:
+                Debug.Log("NEW GAME BUTTON PRESSED");
+                SceneManager.LoadScene("SampleScene");
+                break;
+            case ButtonType.CREDITS:
+                Debug.Log("CREDITS BUTTON PRESSED");
+                SceneManager.LoadScene("Credits");
+                break;
+            case ButtonType.RETURN:
+                Debug.Log("Return BUTTON PRESSED");
+                SceneManager.LoadScene("Start Menu");
+                break;
+            case ButtonType.QUIT:
+                Debug.Log("QUIT BUTTON PRESSED");
+                Application.Quit();
+                break;
+        }
+    }
+}
