b2cde0df0de4ccb0d83e1189e13a550daa39e212
Cristian <carredondo@csumb.edu>
2021-05-13 17:29:10 -0700

    Fixed Chest and Changed Barrier Settings

--- Assets/Scripts/Barrier.cs
+++ Assets/Scripts/Barrier.cs
@@ -9,7 +9,7 @@ public class Barrier : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        if (player.gameObject.GetComponent<PlayerLeveling>().currentLevel == 5)
+        if (player.gameObject.GetComponent<PlayerLeveling>().currentLevel == 6)
         {
             Debug.Log("Barrier is now gone!");
             Destroy(this.gameObject);
